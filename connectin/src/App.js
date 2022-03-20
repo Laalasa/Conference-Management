bfd62d770f6a9f0cf4672704669fd8a09b586287
Laalasa <iit2020178@iiita.ac.in>
2022-03-20 17:12:44 +0530

    Added React Template

---
+++ connectin/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import GetStarted from "./GetStarted";
+
+function App() {
+  return (
+    <div className="App">
+      Hello world
+      <br />
+      <GetStarted />
+    </div>
+  );
+}
+
+export default App;
